97fbe6071b0ac32837ad23c0dade5208c2bfd20c
dsalter1 <dsalter@uarts.edu>
2020-03-12 00:48:24 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var greeting = "Hello, everybody!";
 
 var specialGuest = "Neil deGrasse Tyson"
-
+var "Neil deGrasse Tyson"= "Hello, Neil deGrasse Tyson"
 var greetSpecialGuest = "" + specialGuest + "!";
 
 var topic = "space";
